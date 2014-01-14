7c1dda5da9493b6a9949917475d2bffba7820679
Justin Valentini <justin.valentini@gmail.com>
2014-01-14 15:47:10 -0500

    Add sqlpath for alyssa.

--- .exports.sh
+++ .exports.sh
@@ -1,9 +1,11 @@
 if [[ $HOST == 'alyssa' ]]; then
     # Alyssa exports
-    export ORACLE_HOME="/u01/app/oracle/product/client/11.2.0.4"
+#     export ORACLE_HOME="/u01/app/oracle/product/client/11.2.0.4"
+    export ORACLE_HOME="/u01/app/oracle/product/client/11g"
     export TNS_ADMIN="$ORACLE_HOME/network/admin"
     export SCRIPTS_DIR="$HOME/scripts"
     export PATH="$HOME/.cask/bin:$SCRIPTS_DIR/ruby:$SCRIPTS_DIR/php:$ORACLE_HOME/bin:$HOME/.gems/bin:$HOME/installs/bin:/home/dev/bin:/usr/local/sbin/:$PATH"
+    export SQLPATH="$HOME/.sqlplus:$HOME/git/sqlplus-scripts/"
 else
     export no_proxy=localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16
     export http_proxy=http://13.147.7.31:8000
