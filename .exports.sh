4f12864f838660135677240bd986ce6e6646d955
Justin Valentini <justin.valentini@gmail.com>
2014-01-20 10:13:44 -0500

    Add virtualenv and amici env vars to export.

--- .exports.sh
+++ .exports.sh
@@ -1,11 +1,22 @@
+export AMICI_USERNAME="jvalentini"
+export DEV_USERNAME='jvalentini'
+export APP_USERNAME='jvalentini_amicillccom'
+
 if [[ $HOST == 'alyssa' ]]; then
     # Alyssa exports
-#     export ORACLE_HOME="/u01/app/oracle/product/client/11.2.0.4"
-    export ORACLE_HOME="/u01/app/oracle/product/client/11g"
+    export ORACLE_HOME="/u01/app/oracle/product/client/11.2.0.4"
+#     export ORACLE_HOME="/u01/app/oracle/product/client/11g"
     export TNS_ADMIN="$ORACLE_HOME/network/admin"
+    export LD_LIBRARY_PATH="$ORACLE_HOME/lib"
     export SCRIPTS_DIR="$HOME/scripts"
     export PATH="$HOME/.cask/bin:$SCRIPTS_DIR/ruby:$SCRIPTS_DIR/php:$ORACLE_HOME/bin:$HOME/.gems/bin:$HOME/installs/bin:/home/dev/bin:/usr/local/sbin/:$PATH"
     export SQLPATH="$HOME/.sqlplus:$HOME/git/sqlplus-scripts/"
+
+    # Virtualenv
+    export WORKON_HOME="$HOME/env"
+    export VIRTUALENVWRAPPER_PYTHON="/usr/local/bin/python2.7"
+    export VIRTUALENVWRAPPER_VIRTUALENV="/usr/local/bin/virtualenv-2.7"
+    export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
 else
     export no_proxy=localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16
     export http_proxy=http://13.147.7.31:8000
