34958f0a0fb1238dad7b9ce1b0553363ad748e8e
Justin Valentini <justin.valentini@gmail.com>
2014-04-14 14:43:48 -0400

    Fix infinite symlink loop for pip.conf.
    When building the symlink, the target should be the file in the dotfile
    directory, not the pip conf directory.

--- install.sh
+++ install.sh
@@ -17,10 +17,10 @@ do
 done
 
 xmonad_dir=$HOME/.xmonad
-mkdir -p $xmonad_dir && cd $xmonad_dir && ln -sf "$dotfile_dir/xmonad.hs" xmonad.hs
+mkdir -p $xmonad_dir && cd $xmonad_dir && ln -sf "$dotfile_dir/.xmonad/xmonad.hs" xmonad.hs
 
 pip_dir=$HOME/.pip
-mkdir -p $pip_dir && cd $pip_dir && ln -sf "$pip_dir/pip.conf" pip.conf
+mkdir -p $pip_dir && cd $pip_dir && ln -sf "$dotfile_dir/pip.conf" pip.conf
 
 sqlplus_dir=$HOME/.sqlplus
 mkdir -p $sqlplus_dir && cd $sqlplus_dir && ln -sf "$dotfile_dir/.sqlplus/login.sql" login.sql
