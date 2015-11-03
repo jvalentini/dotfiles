d4a932919e789ebed4f4e801fc42099a3d332851
Justin Valentini <justin.valentini@gmail.com>
2015-11-03 08:48:21 -0500

    Add nodejs to system_install and re-org it.

--- system_install.sh
+++ system_install.sh
@@ -5,12 +5,32 @@ sudo apt-get purge emacs emacs-snapshot-common emacs-snapshot-bin-common emacs-s
 # Add the following with visudo (/etc/sudoers)
 sudo echo 'Defaults env_keep += "http_proxy https_proxy ftp_proxy no_proxy"' >> /etc/sudoers
 
+sudo add-apt-repository ppa:leolik/leolik
+sudo add-apt-repository ppa:amandeepgrewal/notifyosdconfig
+sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -s -c) contrib"
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install -y build-essential software-properties-common python-software-properties terminator curl git corkscrew zsh xmonad xmobar xscreensaver offlineimap gnome-panel xmonad xmobar xscreensaver libaio1 virtualbox vagrant php python python-dev python-pip pylint ipython rbenv ruby-dev redis sqlite pidgin pidgin-plugin-pack openssh-client openssh-server php5
+sudo apt-get install -y build-essential software-properties-common python-software-properties dkms terminator curl git corkscrew zsh xmonad xmobar xscreensaver offlineimap gnome-panel xmonad xmobar xscreensaver libaio1 linux-headers-generic virtualbox-dkms virtualbox-5.0 vagrant php python python-dev python-pip pylint ipython rbenv ruby-dev redis sqlite pidgin pidgin-plugin-pack openssh-client openssh-server php5
+
+sudo apt-get install libnotify-bin
+pkill notify-osd
+sudo apt-get install notifyosdconfig
+
+# zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# chsh -s /bin/zsh
+
+# dotfiles
+git clone https://github.com/jvalentini/dotfiles.git
+cd dotfiles
+bash ./install.sh
+
+sudo dpkg-reconfigure virtualbox-dkms
+sudo dpkg-reconfigure virtualbox
 
 sudo pip install -U pip
 sudo pip install virtualenvwrapper
@@ -27,27 +47,18 @@ sudo apt-get build-dep emacs24
 curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
 rm -rf ~/.emacs.d
 git clone https://github.com/jvalentini/emacs.git .emacs.d/
-
-# ruby
-sudo apt-get -y install libc6-dev libssl-dev libmysql++-dev libsqlite3-dev make build-essential libssl-dev libreadline6-dev zlib1g-dev libyaml-dev
-
-# zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# chsh -s /bin/zsh
-
-# dotfiles
-git clone https://github.com/jvalentini/dotfiles.git
-cd dotfiles
-bash ./install.sh
-
-# TODO!!!!!!!!
+git clone git@gitlab.amicillc.com:dev-tools/amici-el.git ~/.emacs.d/lib/amici-el
+cask
 mkdir -p ~/.erc/logs
 move over ~/.emacs.d/custom/secret.json
 
+# ruby
+sudo apt-get -y install libc6-dev libssl-dev libmysql++-dev libsqlite3-dev make build-essential libssl-dev libreadline6-dev zlib1g-dev libyaml-dev
+# sudo gem install librarian-chef
 
-git clone git@gitlab.amicillc.com:dev-tools/amici-el.git ~/.emacs.d/lib/amici-el
-
-cask
+# node
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 # Download oracle instantclient 11.2 - both the basic package and sqlplus
 # http://www.oracle.com/technetwork/topics/linuxx86-64soft-092277.html
@@ -58,14 +69,3 @@ cd $ORACLE_HOME
 sudo ln -s libclntsh.so.11.1 libclntsh.so
 
 git clone git@gitlab.amicillc.com:omnix-tools/tnsnames.git
-
-sudo add-apt-repository ppa:leolik/leolik
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install libnotify-bin
-pkill notify-osd
-sudo add-apt-repository ppa:amandeepgrewal/notifyosdconfig
-sudo apt-get update
-sudo apt-get install notifyosdconfig
-
-sudo gem install librarian-chef
