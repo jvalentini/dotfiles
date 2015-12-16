968ef55f015dc47bdc26a3fd53d6b98eb1c4c3e5
Justin Valentini <justin.valentini@gmail.com>
2015-12-16 17:28:42 -0500

    Add ansible_init.

--- .exports.sh
+++ .exports.sh
@@ -99,3 +99,33 @@ function authorized_keys() {
     ansible-playbook -i ~/dotfiles/hosts ~/dotfiles/authorized_keys.yml --list-hosts
     ansible-playbook -i ~/dotfiles/hosts ~/dotfiles/authorized_keys.yml -k -f 5
 }
+
+function ansible_init() {
+    git_root=`git rev-parse --show-toplevel`
+    if [ $git_root != $PWD ]
+    then
+        echo 'ERROR: Git project root does not match current directory.'
+        return
+    else
+        _ansible_bootstrap
+    fi
+}
+
+function _ansible_bootstrap() {
+    mkdir -p ansible
+    cd ansible
+    cat <<EOF > playbook.yml
+---
+- name: create user
+  hosts: all
+  sudo: true
+  vars:
+    user: "hades"
+    add_ssh_public_key: true
+  roles:
+    - common
+EOF
+    echo "- src: git+git@gitlab.amicillc.com:dev-tools/ansible-common.git\n  name: common" > roles.yml
+    echo "[defaults]\nroles_path = roles" > ansible.cfg
+    cd ..
+}
